5be70c5909f3e2929fee688fd77becfa31f280c7
mike <mikeengland@users.noreply.github.com>
2018-11-04 18:03:22 +0100

    Bumped version to 1.0.1

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(
     name='django-setty',
 
-    version='1.0.0',
+    version='1.0.1',
 
     author='Michael England',
     author_email='michael.k.england@gmail.com',
