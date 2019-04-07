bef4d5e8bf0184f5f1d3420b3aac3e29644f1efb
mike <mikeengland@users.noreply.github.com>
2019-04-07 23:57:50 +0200

    Bump version to 3.1.0

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
 setup(
     name='django-setty',
 
-    version='3.0.0',
+    version='3.1.0',
 
     author='Michael England',
     author_email='michael.k.england@gmail.com',
