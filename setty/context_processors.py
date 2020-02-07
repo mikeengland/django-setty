26514efb36e2ef82633c9a36fbc6f8916ecfa596
eugene <koc@z-gu.ru>
2020-02-07 11:16:29 +0300

    change tags

--- setty/context_processors.py
+++ setty/context_processors.py
@@ -1,4 +1,6 @@
 import setty
+from django.conf import settings
+from django.urls import resolve
 
 
 def setty_settings(request):
@@ -8,7 +10,8 @@ def setty_settings(request):
     Add 'setty.context_processors.setty_settings' to the
     TEMPLATE_CONTEXT_PROCESSORS setting to ensure this can be used.
     """
-    tags = {f'setty_{app}': setty.config.get_for_app(app) for app in settings.INSTALLED_APPS}
-    tags['setty'] = setty.config
+
+    app_name = resolve(request.path).app_name
+    tags = {'setty_current_app': setty.config.get_for_app(app_name), 'setty': setty.config}
 
     return tags
